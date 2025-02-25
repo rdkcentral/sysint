6b5cbafe617a3b36080c325c0034fa235714daf0
Aravindan NC <35158113+AravindanNC@users.noreply.github.com>
2025-02-25 11:27:05 -0500

    Update NM_Bootstrap.sh

--- lib/rdk/NM_Bootstrap.sh
+++ lib/rdk/NM_Bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-####################################################################################
+##############################################################################
 # If not stated otherwise in this file or this component's LICENSE file the
 # following copyright and licenses apply:
 #
-# Copyright 2024 Comcast Cable Communications Management, LLC
+# Copyright 2020 RDK Management
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-####################################################################################
+##############################################################################
 
 WIFI_WPA_SUPPLICANT_CONF="/nvram/secure/wifi/wpa_supplicant.conf"
 
