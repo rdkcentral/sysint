6bf1f846c04400e890631644a1b90818b4398031
Aravindan NC <35158113+AravindanNC@users.noreply.github.com>
2025-02-25 14:02:25 -0500

    RDKEMW-1929: [RDKE][XUMO]-Unable to REVSSH to Device  (#52)
    * Create NM_Bootstrap.sh
    
    * Create NM_Bootstrap.service
    
    * Update NM_Bootstrap.sh

---
+++ lib/rdk/NM_Bootstrap.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+##############################################################################
+# If not stated otherwise in this file or this component's LICENSE file the
+# following copyright and licenses apply:
+#
+# Copyright 2020 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
+WIFI_WPA_SUPPLICANT_CONF="/nvram/secure/wifi/wpa_supplicant.conf"
+
+if [ -f $WIFI_WPA_SUPPLICANT_CONF ]; then
+  SSID=$(cat $WIFI_WPA_SUPPLICANT_CONF | grep -w ssid= | cut -d '"' -f 2)
+  PSK=$(cat $WIFI_WPA_SUPPLICANT_CONF | grep -w psk= | cut -d '"' -f 2)
+
+  ETH_STATUS=$(nmcli dev status | grep -w eth0)
+  IS_ETH_CONN=$(echo $ETH_STATUS | grep -w connected)
+  WIFI_STATUS=$(nmcli dev status | grep -w wlan0)
+  IS_WIFI_CONN=$(echo $WIFI_STATUS | grep -w connected)
+
+  if [ -z "${IS_ETH_CONN}" ]; then
+          if [ -z "${IS_WIFI_CONN}" ]; then
+            if [ -z "$( ls -A '/etc/NetworkManager/system-connections' )" ]; then
+                  #connect to wifi
+                  nmcli conn add type wifi con-name $SSID autoconnect yes ifname wlan0 ssid $SSID wifi-sec.key-mgmt wpa-psk wifi-sec.psk $PSK 
+                  nmcli conn reload
+            fi
+          fi
+  fi
+fi
