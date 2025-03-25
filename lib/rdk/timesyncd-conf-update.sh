d1d7b42741f2a7c052a1caeb169dfbea544bdc43
nhanasi <navihansi@gmail.com>
2025-03-25 15:33:43 -0400

    Update timesyncd-conf-update.sh

--- lib/rdk/timesyncd-conf-update.sh
+++ lib/rdk/timesyncd-conf-update.sh
@@ -18,60 +18,91 @@
 # limitations under the License.
 ##############################################################################
 
-# Purpose : To update the timesyncd configuration file
-# Scope : RDK Devices
-# Usage : Invoke by systemd service
 
 sleep_time=5
 output=""
 count=0
+LOG_FILE="/opt/logs/ntp.log"
 
 if [ -f /etc/env_setup.sh ]; then
     . /etc/env_setup.sh
 fi
 
-# NTP URL from the property file
-get_ntp_hosts() {
-if [ -f /lib/rdk/getPartnerProperty.sh ]; then
-     hostName=`/lib/rdk/getPartnerProperty.sh ntpHost`
-     hostName2=`/lib/rdk/getPartnerProperty.sh ntpHost2`
-     hostName3=`/lib/rdk/getPartnerProperty.sh ntpHost3`
-     hostName4=`/lib/rdk/getPartnerProperty.sh ntpHost4`
-     hostName5=`/lib/rdk/getPartnerProperty.sh ntpHost5`
-fi
+#Log framework to print timestamp and source script name
+ntpLog()
+{
+    echo "`/bin/timestamp` : $0: $*" >> $LOG_FILE
 }
 
+# Ensure auth service is ready for URL request
+if [ -f /lib/systemd/system/authservice.service ];then
+    auth_pid=`pidof authservice`
+    while [ ! $auth_pid ]
+    do
+         sleep 5
+         auth_pid=`pidof authservice`
+    done
+    ntpLog "Auth service ready now..!"
+    sleep 2
+fi
+
+ntpLog "Retrive NTP Server URL from /lib/rdk/getPartnerProperty.sh..."
 while [ ! "$hostName" ] && [ ! "$hostName2" ] && [ ! "$hostName3" ] && [ ! "$hostName4" ] && [ ! "$hostName5" ]
 do
-   get_ntp_hosts
+    # NTP URL from the property file
+    if [ -f /lib/rdk/getPartnerProperty.sh ]; then
+         hostName=`/lib/rdk/getPartnerProperty.sh ntpHost`
+         hostName2=`/lib/rdk/getPartnerProperty.sh ntpHost2`
+         hostName3=`/lib/rdk/getPartnerProperty.sh ntpHost3`
+         hostName4=`/lib/rdk/getPartnerProperty.sh ntpHost4`
+         hostName5=`/lib/rdk/getPartnerProperty.sh ntpHost5`
+    fi
+
    sleep 5
 done
-echo "NTP Server URL for this env is $hostName ..!"
+
+partnerHostnames="$hostName $hostName2 $hostName3 $hostName4 $hostName5"
+ntpLog "NTP Server URL for the partner:$partnerHostnames"
 
 # Update the timesyncd configuration with URL
 if [ -f /etc/systemd/timesyncd.conf ];then
-      defaultHostName=$(cat /etc/systemd/timesyncd.conf | grep ^NTP= | cut -d "=" -f2 | tr -s ' ')
+      defaultHostName=`cat /etc/systemd/timesyncd.conf | grep ^NTP= | cut -d "=" -f2 | tr -s ' '`
       defaultHostName2=$defaultHostName
       if [ "$hostName" ] || [ "$hostName2" ] || [ "$hostName3" ] || [ "$hostName4" ] || [ "$hostName5" ];then
            if  [ "$hostName" == "$defaultHostName" ] || [ "$hostName2" == "$defaultHostName" ] || [ "$hostName3" == "$defaultHostName" ] || [ "$hostName4" == "$defaultHostName" ] || [ "$hostName5" == "$defaultHostName" ];then
                  defaultHostName2=''
            fi
+           for host in $partnerHostnames; do
+               if grep -q "$host" /etc/systemd/timesyncd.conf; then
+                    updateHostnames+=" "
+               else
+                    updateHostnames+="$host "
+               fi
+           done
+           updateHostname=$(echo "$updateHostnames" | sed 's/^[[:space:]]*//; s/[[:space:]]*$//')
 
-           # Update the timesyncd configuration with URL
-           cp /etc/systemd/timesyncd.conf /tmp/timesyncd.conf
-           sed -i "s/^NTP=$defaultHostName/NTP=$hostName $hostName2 $hostName3 $hostName4 $hostName5 $defaultHostName2/" /tmp/timesyncd.conf
-           echo "ConnectionRetrySec=5" >> /tmp/timesyncd.conf
-           cat /tmp/timesyncd.conf > /etc/systemd/timesyncd.conf
-           rm -rf /tmp/timesyncd.conf
-           # Restart the service to reflect the new conf
-           echo "`date`: Restarting the service: systemd-timesyncd.service ..!"
-           /bin/systemctl reset-failed systemd-timesyncd.service
-           /bin/systemctl restart systemd-timesyncd.service
+           if [ ! -z "$updateHostname" ]; then
+               # Update the timesyncd configuration with URL
+               ntpLog "Update the timesyncd configuration with URL updateHostname=$updateHostname defaultHostName=$defaultHostName2"
+
+               cp /etc/systemd/timesyncd.conf /tmp/timesyncd.conf
+               sed -i "s/^NTP=.*/NTP=/g" /tmp/timesyncd.conf
+               sed -i "s/^NTP=/NTP=$updateHostname $defaultHostName2/" /tmp/timesyncd.conf
+               cat /tmp/timesyncd.conf > /etc/systemd/timesyncd.conf
+               rm -rf /tmp/timesyncd.conf
+
+               # Restart the service to reflect the new conf
+               ntpLog "Restarting the service: systemd-timesyncd.service ..!"
+               /bin/systemctl reset-failed systemd-timesyncd.service
+               /bin/systemctl restart systemd-timesyncd.service
+           else
+               ntpLog "No new Hostnames found to update /etc/systemd/timesyncd.conf"
+           fi
       else
-           echo "Hostnames are same ($hostName, $defaultHostName)"
+           ntpLog "Hostnames are same ($hostName, $defaultHostName)"
       fi
 else
-      echo "Missing configuration file: /etc/systemd/timesyncd.conf"
+      ntpLog "Missing configuration file: /etc/systemd/timesyncd.conf"
 fi
 
 exit 0
