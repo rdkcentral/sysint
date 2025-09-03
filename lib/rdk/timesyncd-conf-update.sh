328fdf8a6d442e5c567b99b6af016a9575d6096f
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2025-09-03 15:41:52 +0530

    Update timesyncd-conf-update.sh

--- lib/rdk/timesyncd-conf-update.sh
+++ lib/rdk/timesyncd-conf-update.sh
@@ -91,9 +91,6 @@ if [ -f /etc/systemd/timesyncd.conf ];then
                rm -rf /tmp/timesyncd.conf
 
                # Restart the service to reflect the new conf
-               ntpLog "Restarting the service: systemd-timesyncd.service ..!"
-               /bin/systemctl reset-failed systemd-timesyncd.service
-               /bin/systemctl restart systemd-timesyncd.service
            else
                ntpLog "No new Hostnames found to update /etc/systemd/timesyncd.conf"
            fi
