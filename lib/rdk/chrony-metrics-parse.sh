694a760f9dd1c313e8de10b7a7631ad236fbf82e
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-03-06 12:26:48 +0530

    Create chrony-metrics-parse.sh

---
+++ lib/rdk/chrony-metrics-parse.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+line=$(tail -1 /opt/logs/chrony/tracking.log)
+LOG_FILE=/opt/logs/chrony.log
+logger()
+{
+ echo "`/bin/timestamp` : $0: $*" >> $LOG_FILE
+}
+
+date=$(echo "$line" | awk '{print $1}')
+time=$(echo "$line" | awk '{print $2}')
+offset=$(echo "$line" | awk '{print $7}')
+drift=$(echo "$line" | awk '{print $5}')
+delay=$(echo "$line" | awk '{print $11}')
+jitter=$(echo "$line" | awk '{print $9}')
+
+logger "NTP response:"
+logger "  response time : ${date} ${time}"
+logger "  offset        : ${offset} sec"
+logger "  drift         : ${drift} ppm"
+logger "  delay         : ${delay} sec"
+logger "  jitter        : ${jitter} sec"
+
