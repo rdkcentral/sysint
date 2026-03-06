ebc0c789377762b9d57b3e8272d0443e59a8e898
sindhu-krishnan <102755514+sindhu-krishnan@users.noreply.github.com>
2026-03-06 13:04:04 +0530

    Update chrony_metrics_parse.sh

--- lib/rdk/chrony_metrics_parse.sh
+++ lib/rdk/chrony_metrics_parse.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
+LOGFILE="/opt/logs/chrony_telemetry.log"
 
-line=$(tail -1 /opt/logs/chrony/tracking.log)
-LOG_FILE=/opt/logs/tracking-telemetry
-logger()
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
 {
- echo "`/bin/timestamp` : $0: $*" >> $LOG_FILE
-}
-date=$(echo "$line" | awk '{print $1}')
-time=$(echo "$line" | awk '{print $2}')
-offset=$(echo "$line" | awk '{print $7}')
-drift=$(echo "$line" | awk '{print $5}')
-delay=$(echo "$line" | awk '{print $11}')
-jitter=$(echo "$line" | awk '{print $9}')
-
-logger "NTP response:"
-logger "  response time : ${date} ${time}"
-logger "  offset        : ${offset} sec"
-logger "  drift         : ${drift} ppm"
-logger "  delay         : ${delay} sec"
-logger "  jitter        : ${jitter} sec"
+echo "$timestamp chrony telemetry:"
+chronyc tracking
+echo ""
+} >> "$LOGFILE"
