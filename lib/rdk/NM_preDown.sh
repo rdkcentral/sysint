accf07c15cc1f9db7aa60a24e40a5462da00a7d1
jincysam87 <167995204+jincysam87@users.noreply.github.com>
2026-05-04 12:50:42 -0400

    Update NM_preDown.sh

--- lib/rdk/NM_preDown.sh
+++ lib/rdk/NM_preDown.sh
@@ -129,26 +129,4 @@ if [ "x$interfaceName" != "x" ] && [ "$interfaceName" != "lo" ]; then
         if [ "$interfaceName" == "wlan0" ]; then
                 rm /tmp/wifi-on
         fi
-        mode4="ipv4"
-        gwip4=$(/sbin/ip -4 route | awk '/default/ { print $3 }' | head -n1 | awk '{print $1;}')
-        imode4=2
-        ipaddr4=$(ifconfig $interfaceName | grep -w inet | awk -F ' ' '{print $2}' | awk -F ':' '{print $2}')
-        echo "IPADDR4 = $ipaddr4" >> /opt/logs/NMMonitor.log
-        mode6="ipv6"
-        ipaddr6=$(ifconfig $interfaceName | grep -w inet6 | grep Global | awk -F " " '{print $3}' | tail -n1 | cut -d '/' -f1)
-        echo "IPADDR6 = $ipaddr6" >> /opt/logs/NMMonitor.log
-        imode6=10
-        gwip6=$(/sbin/ip -6 route | awk '/default/ { print $3 }' | head -n1 | awk '{print $1;}')
-
-        sh /lib/rdk/ipv6addressChange.sh "delete" $mode4 $interfaceName $ipaddr4 "global"
-        sh /lib/rdk/ipv6addressChange.sh "delete" $mode6 $interfaceName $ipaddr6 "global"
-        echo "$DT_TIME ipv6addressChange.sh" >> /opt/logs/NMMonitor.log
-
-        checkDefaultRoute_Delete  $imode4 $interfaceName $ipaddr4 $gwip4 $interfaceName "metric" "delete"
-        checkDefaultRoute_Delete  $imode6 $interfaceName $ipaddr6 $gwip6 $interfaceName "metric" "delete"
-        echo "$DT_TIME checkDefaultRoute_Delete" >> /opt/logs/NMMonitor.log
-
-        update_global_ip_info_delete "delete" "$mode4" "$interfaceName" "$ipaddr4" "global"
-        update_global_ip_info_delete "delete" "$mode6" "$interfaceName" "$ipaddr6" "global"
-        echo "$DT_TIME update_global_ip_info completed" >> /opt/logs/NMMonitor.log
 fi
